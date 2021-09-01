55c78c2da6486808e76fc4588f46099b1080f550
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-01 15:42:39 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const body = document.getElementsByTagName("body")[0];
+const mainChild = body.querySelector("main");
+body.removeChild(mainChild);
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Sam is the champion";
+document.body.append(newHeader);
+
