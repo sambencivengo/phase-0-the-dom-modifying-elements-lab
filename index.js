76d04483ed830e447697d43b72e52c0babff8f41
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-27 08:45:42 -0400

    commit tree 9df885920969df9cbf7f5f2eb784a5722ffb32fe

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const body = document.getElementsByTagName("body")[0];
 const mainChild = body.querySelector("main");
 body.removeChild(mainChild);
 
+
+
 const newHeader = document.createElement("h1");
 newHeader.id = "victory";
 newHeader.innerHTML = "Sam is the champion";
